e73dd29cf06c08fa3dc82406a352ad601ab5e315
David Paunovski <paunovskidavid@gmail.com>
2024-04-09 01:27:35 +0200

    mongodb connected

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 
-const dotImport = dotenv.config();
+dotenv.config();
+
+const mongoDbAuth = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL_AUTH);
+        console.log("----------> Connected to Auth DB!");
+    } catch (error) {
+        throw error;
+    }
+}
 
 app.get('/', (request, response) => {
     response.send("hello from esm module!");
 });
 
-app.listen(process.env.PORT, () => console.log(`Node is avalable on port ${process.env.PORT}.`));
+app.listen(process.env.PORT, () => {
+    mongoDbAuth();
+    console.log(`Node is avalable on port ${process.env.PORT}.`);
+});
